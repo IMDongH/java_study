1a17ea022439c5580ffd94f392f817b58660d1a1
Dong <idh1007@naver.com>
2021-09-29 01:20:58 +0900

    자바 백준10872 재귀

---
+++ src/java_algorithm/recursive1/factorial.java
@@ -0,0 +1,23 @@
+package java_algorithm.recursive1;
+import java.util.Scanner;
+
+
+
+public class factorial {
+    public static int recur( int number)
+    {
+        if(number==0)
+            return 1;
+        return number*recur(number-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        num=input.nextInt();
+        System.out.println( recur(num));
+
+
+    }
+}
