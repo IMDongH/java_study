1aa451d1d85d435c30923523aad31e492c5a7247
Dong <idh1007@naver.com>
2021-11-06 20:04:18 +0900

    socket practice

--- src/Practice/Database.java
+++ src/Practice/Database.java
@@ -57,7 +57,8 @@ public class Database {
     }
 
     //회원가입을 수행하는 메소드. 회원가입에 성공하면 true, 실패하면 false를 반환한다.
-    boolean joinCheck(String _id, String _password, String _nickname, String _email, String _personer_link,String _name) {
+    //닉네임이랑 ID 중복의 경우는 회원가입 버튼을 ID와 닉네임 모두 중복 확인이 되었을때 활성화 하면 되지 않을까 싶은데...
+    boolean join(String _id, String _password, String _nickname, String _email, String _personer_link,String _name) {
         boolean flag = false;	//참거짓을 반환할 flag 변수. 초기값은 false
 
         //매개변수로 받은 각 문자열들을 각 변수에 초기화한다.
@@ -114,8 +115,20 @@ public class Database {
         return flag;
     }
 
+    ResultSet viewRank(){
+        ResultSet msg = null;
+        try {
+        String viewStr = "SELECT * FROM guest order by win desc";
+        msg = stmt.executeQuery(viewStr);
+        }
+        catch(Exception e)
+        {
 
+        }
+        return msg;
+    }
         //데이터베이스에 저장된 자신의 정보를 조회하는 메소드. 조회한 정보들을 String 형태로 반환한다.
+        //vector에 있는 닉네임 사용해서 반복문 활용해서 이거쓰면 될듯
     ResultSet viewInfo(String _nn) {
         ResultSet msg = null;
 
@@ -158,7 +171,7 @@ public class Database {
     }
 
     //전체 회원의 전적을 조회하는 메소드. 모든 회원의 전적을 String 형태로 반환한다.
-    String viewRank() {
+    String viewAll() {
         String msg = "";	//전적을 받을 문자열. 초기값은 ""로 한다.
 
         try {
@@ -169,7 +182,7 @@ public class Database {
             int count = 0;
             while(result.next()) {
                 //기존의 msg에 "닉네임 : n승 n패@" 형태의 문자열을 계속해서 추가한다.
-                msg = msg + result.getString("nickname") + " : " + result.getInt("win") + "승 " + result.getInt("draw") + "무 " + result.getInt("lose") + "패@";
+                msg = msg + result.getString("nickname") + " : " + result.getInt("win") + "승 " + result.getInt("draw") + "무 " + result.getInt("lose") + "패 "+'\n';
                 count++;
             }
             System.out.println("[Server] 전적 조회 성공");	//정상적으로 수행되면 성공을 콘솔로 알린다.
@@ -181,7 +194,8 @@ public class Database {
     }
 
     //한 명의 회원의 전적을 조회하는 메소드. 해당 회원의 전적을 String 형태로 반환한다.
-    String searchRank(String _nn) {
+    //대기실에서 사용자 표시할때 사용하면 될듯
+    String searchUser(String _nn) {
         String msg = "null";	//전적을 받을 문자열. 초기값은 "null"로 한다.
 
         //매개변수로 받은 닉네임을 초기화한다.
